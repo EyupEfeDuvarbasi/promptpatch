145584041e0001fc4f32cf2cd5a78d5a5296d39e
EyupEfeDuvarbasi <eyupefeduvarbasi28@gmail.com>
2026-08-07 12:54:09 +0300

    Include promptcheck command source

---
+++ cmd/promptcheck/main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/EyupEfeDuvarbasi/promptpatch/internal/cli"
+	"github.com/EyupEfeDuvarbasi/promptpatch/internal/config"
+	"github.com/EyupEfeDuvarbasi/promptpatch/internal/gui"
+	"github.com/EyupEfeDuvarbasi/promptpatch/internal/hotkey"
+	"github.com/EyupEfeDuvarbasi/promptpatch/internal/llm"
+)
+
+func main() {
+	if len(os.Args) == 2 && os.Args[1] == "setup-shell" {
+		if err := cli.SetupShell(); err != nil {
+			fail(err)
+		}
+		return
+	}
+	if len(os.Args) == 2 && os.Args[1] == "gui" {
+		gui.Run()
+		return
+	}
+	if len(os.Args) == 2 && os.Args[1] == "hotkey" {
+		if err := hotkey.Run(); err != nil {
+			fail(err)
+		}
+		return
+	}
+	if len(os.Args) == 2 && os.Args[1] == "install-hotkey" {
+		if err := hotkey.Install(); err != nil {
+			fail(err)
+		}
+		return
+	}
+
+	var client *llm.Client
+	if usesLLM(os.Args[1:]) {
+		path, err := config.DefaultPath()
+		if err != nil {
+			fail(err)
+		}
+		resolved, err := config.Resolve(path, os.Stdin, os.Stdout)
+		if err != nil {
+			fail(err)
+		}
+		client = &resolved
+	}
+	if err := cli.Run(context.Background(), os.Args[1:], os.Stdin, os.Stdout, client); err != nil {
+		fail(err)
+	}
+}
+
+func usesLLM(args []string) bool {
+	for _, arg := range args {
+		if arg == "--model" || len(arg) > len("--model=") && arg[:len("--model=")] == "--model=" {
+			return true
+		}
+	}
+	return false
+}
+
+func fail(err error) {
+	fmt.Fprintln(os.Stderr, "promptcheck:", err)
+	os.Exit(1)
+}
